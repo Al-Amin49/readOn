ca162d96fb9ba1aede9347dced369b464ec6f94f
Al-Amin49 <alamin496832@gmail.com>
2024-10-16 00:35:33 +0600

    feat:dynamically card created

--- JS/app.js
+++ JS/app.js
@@ -10,7 +10,35 @@ const fetchBooks = async () => {
 
 function displayBooks(books){
     const bookContainer=document.getElementById('book-container');
-    books.forEach(book=>console.log(book.title))
+    books.forEach(book=>{
+        const bookDiv=document.createElement('div');
+        bookDiv.classList.add('card');
+        bookDiv.innerHTML=`
+        <div class="card">
+          <img
+            src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg"
+            alt="book"
+            class="card-image"
+          />
+          <div class="card-content">
+            <div class="title-container">
+              <h4 class="card-title">${book.title}</h4>
+              <p >Id:${book.id}</p>
+            </div>
+            <p class="authors-name">
+              Lorem ipsum dolor sit, amet consectetur a.
+            </p>
+            <p class="genre">Genre: book</p>
+
+            <a href="">
+              <button class="btn">Show Details</button>
+            </a>
+          </div>
+        </div>
+        
+        `
+        bookContainer.appendChild(bookDiv);
+    })
 }
 
 fetchBooks()
