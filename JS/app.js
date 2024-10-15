33948d9afd9714a6888bacf52aeadf56cfd246d8
Al-Amin49 <alamin496832@gmail.com>
2024-10-15 23:58:48 +0600

    book data fetched

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,16 @@
-const apiUrl = 'https://gutendex.com/books';
-let books = [];
-
 
 // Fetch books from API
 const fetchBooks = async () => {
-    try {
-        const response = await fetch(apiUrl);
-        console.log('response', response);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log('data', data);
-        books = data.results; 
-        console.log('Books fetched successfully:', books);
-        
-    } catch (error) {
-        console.error('Error fetching books:', error);
-    }
+    const res=await fetch('https://gutendex.com/books');
+    const data=await res.json();
+    const books=data.results;
+    displayBooks(books)
+
 };
+
+function displayBooks(books){
+    const bookContainer=document.getElementById('book-container');
+    books.forEach(book=>console.log(book.title))
+}
 
 fetchBooks()
